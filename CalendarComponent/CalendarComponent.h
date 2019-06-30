b3e827d167c40b94eb3fe2ba379a0a0747a9eb4c
Jaafar <jaafar@ionoview.com>
2019-06-30 16:29:52 +0300

    Initial Commit

---
+++ CalendarComponent/CalendarComponent.h
@@ -0,0 +1,19 @@
+//
+//  CalendarComponent.h
+//  CalendarComponent
+//
+//  Created by MATIC on 6/30/19.
+//  Copyright © 2019 MATIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarComponent.
+FOUNDATION_EXPORT double CalendarComponentVersionNumber;
+
+//! Project version string for CalendarComponent.
+FOUNDATION_EXPORT const unsigned char CalendarComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarComponent/PublicHeader.h>
+
+
